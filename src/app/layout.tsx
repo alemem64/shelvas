1d37f0650c12897eb389198cb8f313d02c4f5a10
Jzahnny <alemem64@gmail.com>
2024-09-16 22:31:42 +0900

    ZoomWrapper 상단 그림자 성공

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import { ZoomProvider } from '../context/ZoomContext';
 import { HandModeProvider } from '../context/HandModeContext';
 import ZoomWrapper from '@/components/ZoomWrapper';
 import SidebarShadow from '@/components/Sidebar/SidebarShadow';
+import ZoomWrapperShadow from '@/components/ZoomWrapperShadow';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -35,11 +36,14 @@ export default function RootLayout({
                 <Sidebar />
                 <div className="flex-1 flex flex-col overflow-hidden relative">
                   <Toolbar />
-                  <ZoomWrapper>
-                    <main className="flex-1 overflow-auto bg-[#EBECF0]">
-                      {children}
-                    </main>
-                  </ZoomWrapper>
+                  <div className="relative flex-1">
+                    <ZoomWrapperShadow />
+                    <ZoomWrapper>
+                      <main className="flex-1 overflow-auto bg-[#EBECF0]">
+                        {children}
+                      </main>
+                    </ZoomWrapper>
+                  </div>
                   <div className="absolute left-0 top-0 h-full w-3 z-50 pointer-events-none">
                     <SidebarShadow />
                   </div>
--- src/components/ZoomWrapperShadow.tsx
+++ src/components/ZoomWrapperShadow.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 
 const ZoomWrapperShadow: React.FC = () => {
   return (
-    <>
-      <div className="absolute top-0 left-0 right-0 h-3 bg-gradient-to-b from-gray-900/10 to-transparent z-10" />
-    </>
+    <div className="absolute top-0 left-0 right-0 h-3 bg-gradient-to-b from-gray-900/10 to-transparent z-10 pointer-events-none" />
   );
 };
 
